d9dc9469a8ff7ae8dc32e6104c2b2d9dbc3c2516
seonyo <jsy6535@naver.com>
2024-05-09 01:34:31 +0900

    Fix (#16) : FirebaseConfig databaseURL 누락된 정보 추가

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,11 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 
-const {APIKEY, AUTHDOMAIN, PROJECTID, STORAGEBUCKET, MESSAGINGSENDERID, APPID, MEASUREMENTID} = process.env;
+const {APIKEY, AUTHDOMAIN, DATABASEURL, PROJECTID, STORAGEBUCKET, MESSAGINGSENDERID, APPID, MEASUREMENTID} = process.env;
 
 const firebaseConfig = {
   apiKey: APIKEY,
   authDomain: AUTHDOMAIN,
+  DATABASEURL : DATABASEURL,
   projectId: PROJECTID,
   storageBucket: STORAGEBUCKET,
   messagingSenderId: MESSAGINGSENDERID,
